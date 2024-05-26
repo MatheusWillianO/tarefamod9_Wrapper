f6216f14cd29369897763732963452ad2d6bec6b
Matheus <matheusmail78@gmail.com>
2024-05-26 15:08:48 -0300

    primeiro commit

---
+++ src/br/com/matheus/tarefa.java
@@ -0,0 +1,9 @@
+package br.com.matheus;
+
+public class tarefa {
+    public static void main(String[] args) {
+        int num = 10;
+        Integer numI = num;
+        System.out.println(numI);
+    }
+}
